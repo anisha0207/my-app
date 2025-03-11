9117aebb0c0c34d0f1d8aef2f301ae8d1a8afe4e
Anisha Dasgupta <147002325+anisha0207@users.noreply.github.com>
2025-03-11 01:06:40 -0400

    Update vite.config.js

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: './',  // This will make Vite look in the current directory (which is the root directory for index.html)
+  base: './',  // This will make Vite look in the current directory (which is the root directory for index.html)
   plugins: [preact()],
   resolve: {
     extensions: ['.js', '.jsx'],
