88fa7667db0d1aab44b223726bbd419403cf8a9e
Anisha Dasgupta <147002325+anisha0207@users.noreply.github.com>
2025-03-11 22:04:52 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: './',  // Ensure this is the correct directory for your project
+  plugins: [react()],
+  base: '/my-app2/', // IMPORTANT:  Match your repository name!
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // This is usually the default, but let's be explicit
+    emptyOutDir: true, // Ensure the output directory is clean before each build
   },
 });
+
