1df01f284245bc512b81e54b68ee886242fb3d75
Anisha Dasgupta <147002325+anisha0207@users.noreply.github.com>
2025-03-11 22:16:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-app2/', // IMPORTANT:  Match your repository name!
+  base: '/', // For Netlify root deployment
   build: {
-    outDir: 'dist', // This is usually the default, but let's be explicit
-    emptyOutDir: true, // Ensure the output directory is clean before each build
-  },
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 });
-
